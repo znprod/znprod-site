1e96a5c9a0c5f5c99179df03a7bb3c58805f1742
Maxim Koylo <maxim@MacBook-Pro-Maxim.local>
2020-11-12 23:33:05 +0200

    class component rewrite + formatting

--- site-frontend/src/components/main-menu/MainMenu.tsx
+++ site-frontend/src/components/main-menu/MainMenu.tsx
@@ -1,70 +1,62 @@
-import burger from "../../images/burger.png";
-import {Link} from "react-router-dom";
-import kvadrat from "../../images/kvadrat2.png";
-import React from "react";
+import React, { useEffect, useRef, useState } from 'react';
+import { Link } from 'react-router-dom';
+
+import burger from '../../images/burger.png';
+import kvadrat from '../../images/kvadrat2.png';
+
 import './MainMenu.css';
 
-type State = {
-    isBurgerMenuOpen: boolean
+export function MainMenu() {
+  const [isBurgerMenuOpen, setIsBurgerMenuOpen] = useState(false);
+
+  const wrapperRef = useRef<HTMLElement | null>(null);
+
+  const toggle = () => {
+    setIsBurgerMenuOpen(!isBurgerMenuOpen);
+  };
+
+  const hideBurgerMenu = () => {
+    setIsBurgerMenuOpen(false);
+  };
+
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (
+        wrapperRef.current &&
+        !wrapperRef.current.contains(event.target as Node)
+      ) {
+        hideBurgerMenu();
+      }
+    };
+
+    document.addEventListener('mousedown', handleClickOutside);
+
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, []);
+
+  const links = [
+    { to: '/', text: 'Выпуски' },
+    { to: '/partnership', text: 'Партнерство' },
+    { to: '/subscribe', text: 'Подписаться' },
+    { to: '/about', text: 'О подкасте' },
+  ];
+
+  return (
+    <nav className="AppNav" ref={wrapperRef}>
+      <img className="burger" src={burger} alt="" onClick={toggle} />
+
+      <ul className={isBurgerMenuOpen ? 'burger-open' : 'burger-closed'}>
+        {links.map((link) => (
+          <li key={link.text}>
+            <Link onClick={hideBurgerMenu} to={link.to}>
+              <img src={kvadrat} alt="" />
+              {link.text}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
 }
-
-class MainMenu extends React.Component {
-
-    state:State = {isBurgerMenuOpen: false}
-    private wrapperRef: React.RefObject<HTMLElement>;
-
-    constructor() {
-        super({});
-        this.wrapperRef = React.createRef();
-        this.toggle = this.toggle.bind(this)
-        this.hideBurgerMenu = this.hideBurgerMenu.bind(this)
-        this.handleClickOutside = this.handleClickOutside.bind(this)
-    }
-
-    toggle() {
-        const currentState = this.state.isBurgerMenuOpen;
-        this.setState({ isBurgerMenuOpen: !currentState });
-    }
-
-    hideBurgerMenu() {
-        this.setState({isBurgerMenuOpen: false})
-    }
-
-    componentDidMount() {
-        document.addEventListener('mousedown', this.handleClickOutside)
-    }
-
-    componentWillUnmount() {
-        document.removeEventListener('mousedown', this.handleClickOutside)
-    }
-
-    handleClickOutside(event:Event) {
-        // @ts-ignore
-        if (this.wrapperRef && !this.wrapperRef.current.contains(event.target)){
-            this.hideBurgerMenu()
-        }
-    }
-
-    render() {
-        return <nav className="AppNav" ref={this.wrapperRef}>
-            <img className="burger" src={burger} alt="" onClick={this.toggle}/>
-
-            <ul className={this.state.isBurgerMenuOpen ? "burger-open" : "burger-closed"}>
-                <li>
-                    <Link onClick={this.hideBurgerMenu} to="/"><img src={kvadrat} alt=""/>Выпуски</Link>
-                </li>
-                <li>
-                    <Link onClick={this.hideBurgerMenu}  to="/partnership"><img src={kvadrat} alt=""/>Партнерство</Link>
-                </li>
-                <li>
-                    <Link onClick={this.hideBurgerMenu}  to="/subscribe"><img src={kvadrat} alt=""/>Подписаться</Link>
-                </li>
-                <li>
-                    <Link onClick={this.hideBurgerMenu}  to="/about"><img src={kvadrat} alt=""/>О подкасте</Link>
-                </li>
-            </ul>
-        </nav>
-    }
-}
-
-export default MainMenu
